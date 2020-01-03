abd0ec9b4e681fc40b83e1d11335b850ea27883c
Zin Min Phyoe <zinminphyo075@gmail.com>
2020-01-03 23:23:06 +0630

    First initialize

---
+++ MovieLists/MovieLists/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieLists
+//
+//  Created by Zin Min Phyoe on 1/3/20.
+//  Copyright © 2020 Zin Min Phyoe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
